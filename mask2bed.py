d450736da87f64302bbf795673b4a90a21916033
Clemens Thoelken <5861076+thoelken@users.noreply.github.com>
2019-11-14 15:57:25 +0100

    replaced mask2bed.py with faster version

--- mask2bed.py
+++ mask2bed.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser as AP, FileType
 import sys
+import re
 
 cli = AP()
 cli.add_argument('-s', '--soft_masked', action='store_true',
@@ -10,25 +11,25 @@ args = cli.parse_args()
 
 lowers = str.maketrans('acgt', 'nnnn')
 
-head, index, rep = 'noRef', 0, -1
+
+def print_bed(head, seq, soft_masked=True):
+    if soft_masked:
+        seq = seq.translate(lowers)
+    for m in re.finditer('n+', seq.lower()):
+        print('%s\t%d\t%d' % (head, m.start(), m.end()))
+
+
+head, seq = 'noRef', ''
 for line in args.fasta:
     line = line.rstrip()
     if not line or line.startswith('!'):
         continue
     if line.startswith('>'):
-        if 0 <= rep:
-            print('%s\t%d\t%d' % (head, rep, index))
+        if seq:
+            print_bed(head, seq, args.soft_masked)
         head = line.split()[0][1:]
-        index, rep = 0, -1
+        seq = ''
         continue
-    if args.soft_masked:
-        line = line.translate(lowers)
-    for c in line.lower():
-        if 0 <= rep and c != 'n':
-            print('%s\t%d\t%d' % (head, rep, index))
-            rep = -1
-        elif rep < 0 and c == 'n':
-            rep = index
-        index += 1
-if 0 <= rep:
-    print('%s\t%d\t%d' % (head, rep, index))
+    seq += line
+if seq:
+    print_bed(head, seq, args.soft_masked)
